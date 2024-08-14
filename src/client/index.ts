d2a1857dca909a446946d7b024c3987f8ee38b18
Zeus <cuzimblackhd@gmail.com>
2024-08-14 15:42:44 +0200

    Added sleepless interact to ox_bankig
    Replaced ox_target with sleepless_interact

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,13 +3,17 @@ import targets from '../../data/targets.json';
 import locations from '../../data/locations.json';
 import atms from '../../data/atms.json';
 import { SendTypedNUIMessage, serverNuiCallback } from 'utils';
-import { getLocales } from '@overextended/ox_lib/shared';
+import { getLocales, locale } from '@overextended/ox_lib/shared';
+import {initLocale} from '@overextended/ox_lib/shared'
 import { OxAccountPermissions, OxAccountRoles } from '@overextended/ox_core';
+import { Vector3 } from '@nativewrappers/client';
 
 const usingTarget = GetConvarInt('ox_banking:target', 0) === 1;
 let hasLoadedUi = false;
 let isUiOpen = false;
 let isATMopen = false;
+
+initLocale()
 
 function initUI() {
   if (hasLoadedUi) return;
@@ -73,34 +77,61 @@ if (!usingTarget) {
 }
 
 if (usingTarget) {
-  exports.ox_target.addModel(
-    atms.map((value) => GetHashKey(value)),
-    {
-      name: 'access_atm',
-      icon: 'fa-solid fa-money-check',
-      label: 'Access ATM',
-      onSelect: () => {
-        openATM();
-      },
+  exports.sleepless_interact.addGlobalModel({
+    models: [
+      {model: "prop_fleeca_atm", offset: new Vector3(0, 0, 1.0)},
+      {model: "prop_atm_01", offset: new Vector3(0, 0, 1.0)},
+      {model: "prop_atm_02", offset: new Vector3(0, 0, 1.0)},
+      {model: "prop_atm_03", offset: new Vector3(0, 0, 1.0)},
+    ],
+      id: 'access_atm',
+      options: [
+        {
+          icon: 'fa-solid fa-money-check',
+          label: locale('access_atm'),
+          onSelect: (id: string | number, entity?: number, coords: Vector3, distance: number) => {
+            openATM();
+          },
+        },
+        {
+          icon: 'fa-solid fa-sack-dollar',
+          label: locale('rob_atm'),
+          onSelect: (id: string | number, entity?: number, coords: Vector3, distance: number) => {
+            openATM();
+          },
+        },
+      ],
+      renderDistance: 2.0,
+      activeDistance: 1.0,
     }
   );
 
   for (let i = 0; i < targets.length; i++) {
     const target = targets[i];
 
-    exports.ox_target.addBoxZone({
-      coords: target.coords,
-      size: target.size,
-      rotation: target.rotation,
-      debug: true,
-      interactionDistance: 1.3,
-      drawSprite: true,
+    exports.sleepless_interact.addCoords({
+      id: 'access_bank',
+      coords: [
+        new Vector3(149.7474, -1041.4419, 29.7),
+        new Vector3(421.4, 567.8, 125.0),
+        new Vector3(314.0676, -279.6146, 54.43),
+        new Vector3(-351.0613, -50.4046, 49.30),
+        new Vector3(-1212.3430, -331.1905, 38.05),
+        new Vector3(248.485, 223.2904, 106.4085),
+        new Vector3(-2962.0, 482.9439, 15.9852),
+        new Vector3(1174.9997, 2707.3660, 38.4),
+        new Vector3(-111.7381, 6469.452, 31.9485),
+      ],
+      // size: target.size,
+      // rotation: target.rotation,
+      // debug: true,
+      renderDistance: 5.0,
+      activeDistance: 1.3,
       options: [
         {
-          name: 'access_bank',
           icon: 'fa-solid fa-dollar-sign',
-          label: 'Access bank',
-          onSelect: () => {
+          label: locale('access_bank'),
+          onSelect: (id: string | number, entity?: number, coords: Vector3, distance: number) => {
             openBank();
           },
         },
